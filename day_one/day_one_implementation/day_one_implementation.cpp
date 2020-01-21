8f86a91c725238a197621c9ecd7ef11e9ca2f4be
Liam Treacy <liam.treacy@geoteric.com>
2020-01-21 20:20:12 +0000

    Day one completed. Lots of tidy up to do

--- day_one/day_one_implementation/day_one_implementation.cpp
+++ day_one/day_one_implementation/day_one_implementation.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+#include "sum_frequencies.h"
+
+void getInput(std::string& s, std::vector<int>& freqs)
+{
+    std::ifstream file(s);
+    std::string line;
+    int l = 0;
+    if (file.is_open())
+    {
+        while (std::getline(file, line))
+        {
+            l = std::stoi(line);
+            freqs.push_back(l);
+        }
+    }
+
+    file.close();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string file_name = "C:\\Users\\LTreacy\\Desktop\\input_advent.txt";
+    std::vector<int> freqs;
+
+    getInput(file_name, freqs);
+
+    std::cout << " Sum is " << sumFreq(freqs) << "\n";
+
+    return 0;
 }
