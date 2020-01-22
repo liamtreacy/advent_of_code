3924d293538624da14e29bcb89bda040459bd155
Liam Treacy <liam.treacy@geoteric.com>
2020-01-22 19:57:57 +0000

    Checksum tests passing

--- checksum_test.py
+++ checksum_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 # Get the number of entries with exactly two matching characters
-def get_mults(strings):
+def get_checksum(strings):
     two_count = 0
     three_count = 0
 
@@ -10,6 +10,7 @@ def get_mults(strings):
         found_three = False
 
         for i in range(0, len(s)):
+
             if s.count(str(s[i])) is 2:
                 found_two = True
             if s.count(str(s[i])) is 3:
@@ -22,20 +23,35 @@ def get_mults(strings):
             three_count +=1
             found_three = False
 
-    return two_count
+    return two_count*three_count
 
 
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(get_mults(['a']), 0)
+        self.assertEqual(get_checksum(['a']), 0)
 
     def test_one_double(self):
-        self.assertEqual(get_mults(['aa','b']), 1)
+        self.assertEqual(get_checksum(['aa','b']), 0)
 
     def test_two_doubles(self):
-        self.assertEqual(get_mults(['aa','dd']), 2)
+        self.assertEqual(get_checksum(['aa','dd']), 0)
+
+    def test_one_triple(self):
+        self.assertEqual(get_checksum(['a','bbb']), 0)
+
+    def test_two_triple(self):
+        self.assertEqual(get_checksum(['eee','bbb', 'c']), 0)
+
+    def test_triple_and_double(self):
+        self.assertEqual(get_checksum(['ee','bbb', 'c']), 1)
+
+    def test_mix(self):
+        self.assertEqual(get_checksum(['f', 'ee','bbb', 'pp', 'fff', 'c']), 4)
+
+    def test_bigger_mix(self):
+        self.assertEqual(get_checksum(['f', 'ee','bbb', 'pp', 'fff', 'c', 'ddd', 'qiee', 'poppld', 'hjhjhjhj', 'qqbq']), 15)
 
 if __name__ == '__main__':
     unittest.main()
