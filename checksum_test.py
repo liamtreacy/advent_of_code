e52e2e60b2d212f486d6af82c83fcc20c03cc01d
Liam Treacy <liam.treacy@geoteric.com>
2020-01-22 19:45:37 +0000

    Checksum detecting doubles ok

--- checksum_test.py
+++ checksum_test.py
@@ -1,29 +1,41 @@
 import unittest
 
 # Get the number of entries with exactly two matching characters
-def get_two(strings):
-    count = 0
+def get_mults(strings):
+    two_count = 0
+    three_count = 0
+
     for s in strings:
+        found_two = False
+        found_three = False
 
         for i in range(0, len(s)):
-            print(s)
-            print(s[i])
-            print(s.count(str(s[i])))
-            print('----')
-            if s.count(str([i])) is 2:
-                count += 1
+            if s.count(str(s[i])) is 2:
+                found_two = True
+            if s.count(str(s[i])) is 3:
+                found_three = True
 
-    return count
+        if found_two is True:
+            two_count += 1
+            found_two = False
+        if found_three is True:
+            three_count +=1
+            found_three = False
+
+    return two_count
 
 
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(get_two(['a']), 0)
+        self.assertEqual(get_mults(['a']), 0)
 
     def test_one_double(self):
-        self.assertEqual(get_two(['aa','b']), 1)
+        self.assertEqual(get_mults(['aa','b']), 1)
+
+    def test_two_doubles(self):
+        self.assertEqual(get_mults(['aa','dd']), 2)
 
 if __name__ == '__main__':
     unittest.main()
