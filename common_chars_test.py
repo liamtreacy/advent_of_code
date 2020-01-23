0858bac349ca3c6ffeddcf32a7c2f58a4d092ed8
Liam Treacy <liam.treacy@gmail.com>
2020-01-23 13:42:10 +0000

    Initial commit for day two, part two

---
+++ common_chars_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+def get_common_chars(strings):
+    return strings
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
