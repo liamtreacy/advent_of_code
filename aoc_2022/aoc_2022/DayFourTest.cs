0b03f0d72374d1b1329765942afb5c9300180264
Liam Treacy <liam.treacy@opencastsoftware.com>
2022-12-07 15:03:13 +0000

    WIP

--- aoc_2022/aoc_2022/DayFourTest.cs
+++ aoc_2022/aoc_2022/DayFourTest.cs
@@ -7,7 +7,12 @@ public class DayFourTest
 {
     class Elf
     {
-        public Range Section { get; set; }
+        public int[] r;
+
+        public Elf()
+        {
+            r = new int[2];
+        }
     }
 
     class ElvesPair
@@ -25,26 +30,53 @@ public class DayFourTest
         // first
         var first = t[0].Split('-');
 
-        eff.elf1 = new Elf
-        {
-            Section = new Range(Int32.Parse(first[0]), Int32.Parse(first[1]))
-        };
-
-
+        eff.elf1 = new Elf();
+        eff.elf1.r[0] = Int32.Parse(first[0]);
+        eff.elf1.r[1] = Int32.Parse(first[1]);
 
         // second
+        
+        var second = t[1].Split('-');
 
-    return eff;
+        eff.elf2 = new Elf();
+        eff.elf2.r[0] = Int32.Parse(second[0]);
+        eff.elf2.r[1] = Int32.Parse(second[1]);
+
+        return eff;
     }
 
-    [Test]
-    public void Parse_Input_Test()
+    bool ContainsSubSection(ElvesPair p)
     {
-        var s = "2-7,4-9";
+        return (p.elf1.r[0] <= p.elf2.r[0]) && (p.elf1.r[1] >= p.elf2.r[1]) ||
+               (p.elf2.r[0] <= p.elf1.r[0]) && (p.elf2.r[1] >= p.elf1.r[1]);
+    }
 
+
+
+    [Test]
+    public void Elf_Section_Doesnt_Contains_The_Other()
+    {
+        var s = "2-6,7-9";
         var f = ParseInput(s);
-        
-        Assert.That(f.elf1.Section, Is.EqualTo(new Range(2,7)));
-        //Assert.That(f.elf2.Section, Is.EqualTo(new Range(4,9)));
+
+        Assert.That(ContainsSubSection(f), Is.EqualTo(false));
+    }
+    
+    [Test]
+    public void Elf_Section_Does_Contains_The_Other()
+    {
+        var s = "2-10,4-7";
+        var f = ParseInput(s);
+
+        Assert.That(ContainsSubSection(f), Is.EqualTo(true));
+    }
+    
+    [Test]
+    public void Elf_Section_Does_Contains_The_Other_Two()
+    {
+        var s = "10-15,9-16";
+        var f = ParseInput(s);
+
+        Assert.That(ContainsSubSection(f), Is.EqualTo(true));
     }
 }
