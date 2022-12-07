27d2ae42ae47899395ac65abd334b4770b80ff27
Liam Treacy <liam.treacy@opencastsoftware.com>
2022-12-07 10:32:41 +0000

    Priority tests

--- aoc_2022/aoc_2022/DayThreeTest.cs
+++ aoc_2022/aoc_2022/DayThreeTest.cs
@@ -9,7 +9,24 @@ public class DayThreeTest
     {
         return (s.Substring(0,s.Length/2), s.Substring(s.Length / 2));
     }
-    
+
+    public char FindCommonType(String s1, String s2)
+    {
+        return s1.Intersect(s2).First();
+    }
+
+    public int GetPriority(char c)
+    {
+        if (char.IsUpper(c))
+        {
+            return (int)c - 38;
+        }
+        else
+        {
+            return (int)c - 96;
+        }
+    }
+
     [Test]
     public void Split_Compartment_Test()
     {
@@ -19,5 +36,16 @@ public class DayThreeTest
         
         Assert.That(t1, Is.EqualTo("vJrwpWtwJgWr"));
         Assert.That(t2, Is.EqualTo("hcsFMMfFFhFp"));
+        
+        Assert.That(FindCommonType(t1,t2), Is.EqualTo('p'));
+    }
+
+    [Test]
+    public void Priority_Lower_Test()
+    {
+        Assert.That(GetPriority('a'), Is.EqualTo(1));
+        Assert.That(GetPriority('z'), Is.EqualTo(26));
+        Assert.That(GetPriority('Z'), Is.EqualTo(52));
+        Assert.That(GetPriority('A'), Is.EqualTo(27));
     }
 }
