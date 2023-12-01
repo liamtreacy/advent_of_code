f320054d808d791c201bf2eb2ef9787ef509327c
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-12-01 09:53:36 +0000

    Combined methods

--- 2023/DayOne.Tests/UnitTest1.cs
+++ 2023/DayOne.Tests/UnitTest1.cs
@@ -8,69 +8,6 @@ public class UnitTest1
         var sut = new CalibrationCalculator();
 
         var s = "aa";
-        Assert.Equal(-1, sut.GetFirstDigit(s));
-    }
-
-    [Fact]
-    public void FindFirstDigitStringWithOneNumber()
-    {
-        var sut = new CalibrationCalculator();
-
-        var s = "5";
-        //Console.WriteLine($"\n=====\n{sut.GetFirstDigit(s)}\n=====\n");
-        Assert.Equal(5, sut.GetFirstDigit(s));
-    }
-
-    
-    [Fact]
-    public void FindFirstDigitStringWithOneNumberAndLetters()
-    {
-        var sut = new CalibrationCalculator();
-
-        var s = "abc8def";
-
-        Assert.Equal(8, sut.GetFirstDigit(s));
-    }
-
-    [Fact]
-    public void FindLastDigitStringWithNoNumbersReturnsMinusOne()
-    {
-        var sut = new CalibrationCalculator();
-
-        var s = "aa";
-        Assert.Equal(-1, sut.GetLastDigit(s));
-    }
-
-    [Fact]
-    public void FindLastDigitStringWithOneNumber()
-    {
-        var sut = new CalibrationCalculator();
-
-        var s = "456";
-  
-        Assert.Equal(6, sut.GetLastDigit(s));
-    }
-
-    
-    [Fact]
-    public void FindLastDigitStringWithOneNumberAndLetters()
-    {
-        var sut = new CalibrationCalculator();
-
-        var s = "abc8de9f";
-
-        Assert.Equal(9, sut.GetLastDigit(s));
-    }
-
-
-
-
-    [Fact]
-    public void FindFirstDigitStringWithNoNumbersReturnsMinusOne1()
-    {
-        var sut = new CalibrationCalculator();
-
-        var s = "aa";
 
         int first, last = -1;
         sut.GetFirstAndLastDigits(s, out first, out last);
@@ -79,7 +16,7 @@ public class UnitTest1
     }
 
     [Fact]
-    public void FindFirstDigitStringWithOneNumber1()
+    public void FindFirstDigitStringWithOneNumber()
     {
         var sut = new CalibrationCalculator();
 
@@ -93,7 +30,7 @@ public class UnitTest1
 
     
     [Fact]
-    public void FindFirstDigitStringWithOneNumberAndLetters1()
+    public void FindFirstDigitStringWithOneNumberAndLetters()
     {
         var sut = new CalibrationCalculator();
 
--- 2023/DayOne/CalibrationCalculator.cs
+++ 2023/DayOne/CalibrationCalculator.cs
@@ -2,38 +2,6 @@ namespace DayOne;
 
 public class CalibrationCalculator
 {
-    public int GetFirstDigit(string s)
-    {
-        string numStr = "";
-
-        foreach(var c in s)
-        {
-            if(char.IsDigit(c))
-                numStr += c;
-        }
-
-        if(numStr == "")
-            return -1;
-
-        return int.Parse(numStr[0].ToString());
-    }
-
-    public int GetLastDigit(string s)
-    {
-        string numStr = "";
-
-        foreach(var c in s)
-        {
-            if(char.IsDigit(c))
-                numStr += c;
-        }
-
-        if(numStr == "")
-            return -1;
-
-        return int.Parse(numStr[numStr.Length-1].ToString());
-    }
-
     public void GetFirstAndLastDigits(in string s, out int first, out int last)
     {
         string numStr = "";
