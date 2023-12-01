dbebee01e5e29b61437e7da86beba18ef2301caa
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-12-01 09:53:02 +0000

    Combined methods

--- 2023/DayOne.Tests/UnitTest1.cs
+++ 2023/DayOne.Tests/UnitTest1.cs
@@ -32,7 +32,7 @@ public class UnitTest1
         Assert.Equal(8, sut.GetFirstDigit(s));
     }
 
-        [Fact]
+    [Fact]
     public void FindLastDigitStringWithNoNumbersReturnsMinusOne()
     {
         var sut = new CalibrationCalculator();
@@ -60,5 +60,48 @@ public class UnitTest1
         var s = "abc8de9f";
 
         Assert.Equal(9, sut.GetLastDigit(s));
+    }
+
+
+
+
+    [Fact]
+    public void FindFirstDigitStringWithNoNumbersReturnsMinusOne1()
+    {
+        var sut = new CalibrationCalculator();
+
+        var s = "aa";
+
+        int first, last = -1;
+        sut.GetFirstAndLastDigits(s, out first, out last);
+        Assert.Equal(-1, first);
+        Assert.Equal(-1, last);
+    }
+
+    [Fact]
+    public void FindFirstDigitStringWithOneNumber1()
+    {
+        var sut = new CalibrationCalculator();
+
+        var s = "5";
+
+        int first, last = -1;
+        sut.GetFirstAndLastDigits(s, out first, out last);
+        Assert.Equal(5, first);
+        Assert.Equal(5, last);
+    }
+
+    
+    [Fact]
+    public void FindFirstDigitStringWithOneNumberAndLetters1()
+    {
+        var sut = new CalibrationCalculator();
+
+        var s = "abc8d6e7f";
+
+        int first, last = -1;
+        sut.GetFirstAndLastDigits(s, out first, out last);
+        Assert.Equal(8, first);
+        Assert.Equal(7, last);
     }
 }
--- 2023/DayOne/CalibrationCalculator.cs
+++ 2023/DayOne/CalibrationCalculator.cs
@@ -33,4 +33,25 @@ public class CalibrationCalculator
 
         return int.Parse(numStr[numStr.Length-1].ToString());
     }
+
+    public void GetFirstAndLastDigits(in string s, out int first, out int last)
+    {
+        string numStr = "";
+
+        foreach(var c in s)
+        {
+            if(char.IsDigit(c))
+                numStr += c;
+        }
+
+        if(numStr == "")
+        {
+            first =-1;
+            last = -1;
+            return;
+        }
+
+        first = int.Parse(numStr[0].ToString());
+        last = int.Parse(numStr[numStr.Length-1].ToString());
+    }
 }
