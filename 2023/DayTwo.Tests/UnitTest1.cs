550ed9421b6ac68f4cee483a61eabfff0be15444
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-12-07 10:10:35 +0000

    Get game id tests pass

--- 2023/DayTwo.Tests/UnitTest1.cs
+++ 2023/DayTwo.Tests/UnitTest1.cs
@@ -3,6 +3,23 @@ namespace DayTwo.Tests;
 public class UnitTest1
 {
     [Fact]
+    public void SplitLineIntoRoundStrings()
+    {
+        string s = "Game 1: 1 blue, 2 green, 3 red; 7 red, 8 green; 1 green, 2 red, 1 blue; 2 green, 3 red, 1 blue; 8 green, 1 blue";
+
+        var sut = new Transformer();
+        var ret = sut.GetRoundStrings(s);
+        string[] expected = {   "1 blue, 2 green, 3 red",
+                                "7 red, 8 green",
+                                "1 green, 2 red, 1 blue",
+                                "2 green, 3 red, 1 blue",
+                                "8 green, 1 blue"
+                            };
+
+        Assert.Equal(expected, ret);
+    }
+
+    [Fact]
     public void SplitLineIntoGame_Id()
     {
         string s = "Game 1: 1 blue, 2 green, 3 red; 7 red, 8 green; 1 green, 2 red, 1 blue; 2 green, 3 red, 1 blue; 8 green, 1 blue";
--- 2023/DayTwo/DayTwo.cs
+++ 2023/DayTwo/DayTwo.cs
@@ -8,13 +8,15 @@ public class Transformer
 {
     public int GetGameId(string s)
     {
-        // find first space
         int spaceIdx = s.IndexOf(' ');
-        // find first :
         int colonIdx = s.IndexOf(':');
-        // Id between them
         var idStr = s.Substring(spaceIdx+1, colonIdx-(spaceIdx+1));
 
         return int.Parse(idStr);
+    }
+
+    public string[] GetRoundStrings(string s)
+    {
+        return "";
     }
 }
