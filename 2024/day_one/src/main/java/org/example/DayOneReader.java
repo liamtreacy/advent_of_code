402fb2c512e67c22f3796af7c3b5b9dc36efed01
Liam Treacy <liam.treacy@opencastsoftware.com>
2025-01-02 14:04:02 +0000

    Part one done

--- 2024/day_one/src/main/java/org/example/DayOneReader.java
+++ 2024/day_one/src/main/java/org/example/DayOneReader.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class DayOneReader {
@@ -7,7 +10,23 @@ public class DayOneReader {
     ArrayList left = new ArrayList<Integer>();
     ArrayList right = new ArrayList<Integer>();
     public DayOneReader(){
-        var fileLocation = "~/Desktop/tmpFile";
+        var fileLocation = "/Users/liam.treacy/Desktop/tmpFile";
+        parseFile(fileLocation, left, right);
+    }
+
+    public static void parseFile(String filePath, ArrayList<Integer> leftList, ArrayList<Integer> rightList) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("\\s+");
+                if (parts.length == 2) {
+                    leftList.add(Integer.parseInt(parts[0]));
+                    rightList.add(Integer.parseInt(parts[1]));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public ArrayList<Integer> GetL(){
